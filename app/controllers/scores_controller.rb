ca37afc8a8cc7ec7b5ea6f4ec8068c431cd94cdc
Shuya Fuchigami <>
2019-01-17 13:39:32 +0100

    fix layout, ruby implementation for ranking

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -54,11 +54,57 @@ class ScoresController < ApplicationController
     ##@scores = Score.order :score ##ascendent
     @scores = Score.order("score DESC") ##descendent
 
+    @scores_desc = @scores
+    ##for s in @scores_desc do
+    @table = []
+    @scores_desc.each_with_index do |s, i|
+      id = s.id
+      sc = s.score
+      p = s.player
+      ca = s.created_at
+      ua = s.updated_at
+      row = {"rank":i+1,"id":id, "score":sc, "player":p, "created_at":ca, "updated_at":ua}
+      @table.push(@row)
+    end
+
     ##sorted with updated_at DESC row, descendent
     @scores_updated_at_desc = Score.order("updated_at DESC")
 
-    ##a = "abc"
-    ##@scores_updated_at_desc.put[a]
+    j_rank = []
+    i_index = []
+    @scores_updated_at_desc_with_ranking = []
+    @scores_updated_at_desc.each_with_index do |s_up,i|
+      puts "======"
+      s_up_id = s_up.id
+      puts "s_up_id: " + s_up_id.to_s + ", " + i.to_s
+      ##puts s_d_id.to_s + ", " + i.to_s
+      i_index.push(s_up_id)
+
+      puts "------"
+      @scores_desc.each_with_index do |s_d,j|
+        s_d_id = s_d.id
+        #puts "s_u_id*" + s_u_id.to_s + ", " + j.to_s
+
+        puts "s_d_id::" + s_d_id.to_s + ", s_up_id:" + s_up_id.to_s + ", jPos:" + j.to_s
+        ##if s_up_id == s_d_id then
+        if s_up.id.to_i == s_d.id.to_i then ## to_iしないと、.idだけでは、tableのrow列そのものを引っ張ってきてしまう。
+          #j_rank.push([s_d_id, s_u_id, j])
+          puts "MATCH!!"
+          id = s_d.id
+          sc = s_d.score
+          p = s_d.player
+          ca = s_d.created_at
+          ua = s_d.updated_at
+          row = {"rank":j+1,"id":id, "score":sc, "player":p, "created_at":ca, "updated_at":ua}
+          @scores_updated_at_desc_with_ranking.push(row)
+          break
+        end
+      end
+    end
+    ##puts "j_rank"
+    ##puts j_rank
+
+    ##debug
 
     ##score_n_d = Score.order("updated_at DESC")
     ##score_n_d.each_with_index do |s, i|
