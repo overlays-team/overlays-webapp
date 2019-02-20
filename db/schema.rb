45affc5d957beaf2bbde63de5215acb1c9b6d08c
Shuya Fuchigami <>
2019-02-20 11:57:11 +0100

    clean up

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_12_15_143516) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "scores", force: :cascade do |t|
     t.integer "score"
     t.string "player"
